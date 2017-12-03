00d9c14c45ba71abc28710c884640f080e26a740
Dayon <seegalbird@mail.com>
2017-12-03 17:41:28 -0600

    Commit for react-native app - WIP

--- node_modules/react-native-navigation/android/app/src/main/java/com/reactnativenavigation/params/SideMenuParams.java
+++
@@ -1,8 +0,0 @@
-package com.reactnativenavigation.params;
-
-import com.reactnativenavigation.views.SideMenu;
-
-public class SideMenuParams extends BaseScreenParams {
-    public boolean disableOpenGesture;
-    public SideMenu.Side side;
-}
